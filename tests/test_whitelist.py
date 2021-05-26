0d536aaa5ff342a750483c10337c2a917ccc0bf4
Daisuke Taniwaki <daisuketaniwaki@gmail.com>
2021-05-26 20:51:11 +0900

    Add tests

--- tests/test_whitelist.py
+++ tests/test_whitelist.py
@@ -5,12 +5,26 @@ from jupyter_env_whitelist.whitelist import filter_env
 
 
 def test_filter_env():
-    pass
-
     with patch.dict(os.environ, {"foo": "1", "bar": "2", "PWD": "pwd"}, clear=True):
         filter_env()
         assert os.environ == {"PWD": "pwd"}
 
     with patch.dict(os.environ, {"JUPYTER_ENV_WHITELIST": "foo,bar", "foo": "1", "bar": "2", "PWD": "pwd"}, clear=True):
+        filter_env()
+        assert os.environ == {"foo": "1", "bar": "2"}
+
+
+def test_filter_env_with_extra_whitelist():
+    with patch.dict(
+        os.environ, {"JUPYTER_ENV_EXTRA_WHITELIST": "foo", "foo": "1", "bar": "2", "PWD": "pwd"}, clear=True
+    ):
+        filter_env()
+        assert os.environ == {"foo": "1", "PWD": "pwd"}
+
+    with patch.dict(
+        os.environ,
+        {"JUPYTER_ENV_WHITELIST": "foo", "JUPYTER_ENV_EXTRA_WHITELIST": "bar", "foo": "1", "bar": "2", "PWD": "pwd"},
+        clear=True,
+    ):
         filter_env()
         assert os.environ == {"foo": "1", "bar": "2"}
